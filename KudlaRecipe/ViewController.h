1f81ec8bad34eeff9d3cb1b968976a216cb8ad06
prajothdsa <prajothdsa@gmail.com>
2015-09-01 12:48:53 +0530

    Initial Commit

---
+++ KudlaRecipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KudlaRecipe
+//
+//  Created by Prajoth Antonio D on 01/09/15.
+//  Copyright (c) 2015 PADStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KudlaRecipe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KudlaRecipe
+//
+//  Created by Prajoth Antonio D on 01/09/15.
+//  Copyright (c) 2015 PADStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
